706d641a1dbee45d5cf9405e253ce41c23a6afb5
Andy <andrewcuccinello@gmail.com>
2020-06-07 02:12:39 -0400

    deleting .js files to be gitignored

--- module/main.js
+++
@@ -1,8 +0,0 @@
-import { Import } from "./apps/import-form";
-Hooks.on("renderItemDirectory", (app, html) => {
-    const button = $("<button>Import Chummer Data</button>");
-    html.find("footer").before(button);
-    button.on("click", (event) => {
-        new Import().render(true);
-    });
-});
